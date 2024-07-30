8b59da69a52c5bb55a02e35f7f8663cc82ec4a4b
Leonardo <leonardovitorio007@gmail.com>
2024-07-30 10:47:52 -0300

    atv-11arrays

---
+++ arrays11.js
@@ -0,0 +1,7 @@
+//Utilize a lista de filmes do exercício anterior e exiba no console cada um dos filmes informados usando um for..of.
+
+const filmes = ["O Poderoso Chefão", "invocacao do mal", "O Senhor dos Anéis", "Matrix", "velozes e furiosos"];
+
+for (const filme of filmes) {
+    console.log(filme);
+}
