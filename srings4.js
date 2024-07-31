8fa78bf42c6d8e7fef2a22296ffa1e2f008230a0
Leonardo <leonardovitorio007@gmail.com>
2024-07-31 11:32:06 -0300

    atv-4strings

---
+++ srings4.js
@@ -0,0 +1,8 @@
+//Crie uma string com uma frase e use o método `toUpperCase` para imprimir a frase em letras maiúsculas.
+
+
+const frase = 'Hoje tem jogo do timão!';
+
+const fraseMaiuscula = frase.toUpperCase();
+
+console.log(fraseMaiuscula); // HOJE TEM JOGO DO TIMÃO!
