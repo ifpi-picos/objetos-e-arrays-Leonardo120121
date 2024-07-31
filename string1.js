74867eb3d81f95afc40ab43833c38180de363b7a
Leonardo <leonardovitorio007@gmail.com>
2024-07-31 11:14:46 -0300

    atv-1string

---
+++ string1.js
@@ -0,0 +1,8 @@
+//Crie uma string com o seu nome e imprima o número de caracteres.
+
+const nome = 'Leonardo';
+console.log(nome.length); // 19
+
+const nomeCompleto =  "Leonardo Vitorio da Silva!"
+console.log(nomeCompleto.length); // 19
+
